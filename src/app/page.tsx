ba852d9ef07517280517e31da21a10a5f15c2af2
debadeep sen <debadeep.sen@streamlinedigital.com>
2024-07-01 21:37:43 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ import Game from '@/components/Game'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Sudoku Game</h1>
-      <h4>Coming Soon!</h4>
+      <div className='text-center'>
+        <h1>Sudoku Game</h1>
+        <p>Coming Soon!</p>
+      </div>
       {/* <Game /> */}
     </main>
   )
