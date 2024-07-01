abd56839f3aed55d1a6953e29587b4fe90104992
debadeep sen <debadeep.sen@streamlinedigital.com>
2024-07-01 21:34:50 +0530

    coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ import Game from '@/components/Game'
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Game />
+      <h1>Sudoku Game</h1>
+      <h4>Coming Soon!</h4>
+      {/* <Game /> */}
     </main>
   )
 }
