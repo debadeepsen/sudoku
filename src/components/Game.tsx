6a85a5a557e68fe6ebc259028c8b3720dc7f0150
debadeep sen <debadeep.sen@streamlinedigital.com>
2024-07-02 02:40:34 +0530

    grid display

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,13 @@
 import { generateSudokuBoard } from '@/app/server-actions/actions'
-import { Box } from '@mui/material'
+import { Board } from '@/lib/generator'
+import { Box, Button, Grid } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 
 const Game = () => {
-  const [sudoku, setSudoku] = useState<any | null>(null)
+  const [sudoku, setSudoku] = useState<{
+    puzzle: Board
+    solution: Board
+  } | null>(null)
 
   const loadData = async () => {
     const data = await generateSudokuBoard(20)
@@ -15,8 +19,52 @@ const Game = () => {
   }, [])
 
   return (
-    <Box className='rounded-sm border bg-zinc-800 w-[65vw] max-w-[1000px]'>
-      <pre>{JSON.stringify(sudoku, null, 2)}</pre>
+    <Box className='rounded-md shadow-lg dark:shadow-black/30 border bg-white dark:bg-forest-800 w-[75vw] p-6 box-border'>
+      {/* <pre>{JSON.stringify(sudoku, null, 2)}</pre> */}
+      <Box className='flex flex-col lg:flex-row'>
+        <Box className='bg-zinc-900/20 dark:bg-zinc-300/20 p-[1px] box-border'>
+          {sudoku?.puzzle.map((row, r) => {
+            return (
+              <Box className='flex' key={r}>
+                {row.map((val, c) => {
+                  return (
+                    <button
+                      key={c}
+                      className='flex justify-center items-center w-[48px] h-[48px] border border-solid border-zinc-500/30 box-border hover:bg-forest-500/20 text-base text-forest-700 dark:text-forest-100 bg-white dark:bg-forest-800'
+                    >
+                      {val || ''}
+                    </button>
+                  )
+                })}
+              </Box>
+            )
+          })}
+        </Box>
+        <Box className='absolute pointer-events-none'>
+          {[0, 1, 2].map(i => (
+            <Box
+              key={i}
+              className='w-[432px] h-[144px] box-border border-b-zinc-950 dark:border-b-zinc-200'
+              position={'relative'}
+              top={1}
+              borderBottom={i === 2 ? 0 : '2px solid'}
+            ></Box>
+          ))}
+        </Box>
+        <Box className='absolute flex pointer-events-none'>
+          {[0, 1, 2].map(i => (
+            <Box
+              key={i}
+              className='w-[144px] h-[432px] box-border border-r-zinc-950 dark:border-r-zinc-200'
+              position={'relative'}
+              left={1}
+              top={1}
+              borderRight={i === 2 ? 0 : '2px solid'}
+            ></Box>
+          ))}
+        </Box>
+        <Box></Box>
+      </Box>
     </Box>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        forest: {
+          50: "#F0F1EF",
+          100: "#E3E5E1",
+          200: "#BFC3BB",
+          300: "#91998A",
+          400: "#444940",
+          500: "#3D4139",
+          600: "#353932",
+          700: "#2E312B",
+          800: "#262924",
+          900: "#191B18",
+          950: "#191B18"
+        }
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
